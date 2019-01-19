81a497fa5245ba0dd7835c75711d2e9720aa73f5
Radoslaw Lemiec <radoslaw.lemiec@gmail.com>
2019-01-19 12:55:51 +0100

    Crypto data loader

---
+++ data/CryptoDataLoader.py
@@ -0,0 +1,17 @@
+import csv
+import pandas as pd
+
+FILE_PATH = "crypto dataset/BTC-USD_2015-2018_1min/gemini_BTCUSD_2018_1min.csv"
+
+
+def load_data():
+    with open(FILE_PATH) as data_file:
+        reader = csv.reader(data_file, delimiter=',')
+        headers = next(reader)
+
+        crypto_data = pd.DataFrame(columns=headers)
+        crypto_data.set_index(headers[0])
+
+        for row in reader:
+            crypto_data.loc[row[0]] = row[1:]
+    return crypto_data

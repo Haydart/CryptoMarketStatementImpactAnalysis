15e2bfb3dbf5b0ee156d5727b0d3a91728b29059
Radoslaw Lemiec <radoslaw.lemiec@gmail.com>
2019-01-21 21:46:20 +0100

    Working model

--- experiments/experiment_def.py
+++ experiments/experiment_def.py
@@ -63,11 +63,11 @@ def run_expreminent(look_forward, hidden_size, batch_size, epochs, dropout, data
     train_x, test_x = split(x)
     train_y, test_y = split(y)
 
-    train_x = np.reshape(train_x, (train_x.shape[0], look_forward, train_x.shape[1]))
-    test_x = np.reshape(test_x, (test_x.shape[0], look_forward, test_x.shape[1]))
+    train_x = np.reshape(train_x, (-1, look_forward, train_x.shape[1]))
+    test_x = np.reshape(test_x, (-1, look_forward, test_x.shape[1]))
 
     model = create_model(hidden_size=hidden_size, look_forward=look_forward, dropout=dropout)
-    model = train(model, train_x, train_y, batch_size=batch_size, epochs=epochs)
+    model = train(model, train_x, train_y, test_x, test_y, batch_size=batch_size, epochs=epochs)
     y_pred = test(model, test_x)
     score = evaluate(test_y, y_pred)
     print('Test Score: %.2f RMSE' % (score))
@@ -75,4 +75,4 @@ def run_expreminent(look_forward, hidden_size, batch_size, epochs, dropout, data
 
 
 dataset = load_dataset("..//data//window-720.csv")
-run_expreminent(look_forward=1, hidden_size=4, batch_size=50, epochs=100, dropout=0.2, dataset=dataset)
+run_expreminent(look_forward=1, hidden_size=10, batch_size=200, epochs=10000, dropout=0.3, dataset=dataset)
--- model/model.py
+++ model/model.py
@@ -1,22 +1,27 @@
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 from sklearn.metrics import mean_squared_error
-import keras
-import tensorflow
+from matplotlib import pyplot
 import math
 
 
 def create_model(hidden_size, look_forward, dropout):
     model = Sequential()
-    model.add(LSTM(hidden_size, return_sequences=True, input_shape=(look_forward, 3)))
-    model.add(Dropout(dropout))
+    model.add(LSTM(hidden_size, input_shape=(look_forward, 3)))
+ #   model.add(Dropout(dropout))
     model.add(Dense(1))
     return model
 
 
-def train(model, train_x, train_y, batch_size, epochs):
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(x=train_x, y=train_y, epochs=epochs, batch_size=batch_size)
+def train(model, train_x, train_y, test_x, test_y, batch_size, epochs):
+
+    model.compile(loss='mae', optimizer='adam')
+    history = model.fit(x=train_x, y=train_y, epochs=epochs, batch_size=batch_size, verbose=2, shuffle=False,
+                        validation_data=(test_x, test_y))
+    pyplot.plot(history.history['loss'], label='train')
+    pyplot.plot(history.history['val_loss'], label='test')
+    pyplot.legend()
+    pyplot.show()
     return model
 
 

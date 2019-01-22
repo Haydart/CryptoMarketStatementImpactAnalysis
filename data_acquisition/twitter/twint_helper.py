fea9ade57dfce22973c0a5a0fe32e1b63cd3424e
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-22 04:24:52 +0100

    Print joined handles batch

--- data_acquisition/twitter/twint_helper.py
+++ data_acquisition/twitter/twint_helper.py
@@ -6,4 +6,5 @@ with open("unique_twitter_handles.csv") as twitter_handles_file:
     all_handles = twitter_handles_file.readlines()
 
     for i in range(int(math.ceil(len(all_handles) / HANDLES_BATCH_SIZE))):
-        print(all_handles[i * HANDLES_BATCH_SIZE:(i + 1) * HANDLES_BATCH_SIZE])
+        handles_batch = all_handles[i * HANDLES_BATCH_SIZE:(i + 1) * HANDLES_BATCH_SIZE]
+        print(','.join(handles_batch))
